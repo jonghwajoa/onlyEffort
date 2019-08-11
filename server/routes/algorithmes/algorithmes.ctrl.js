6b1eb8287f34b526370ffd352e9cafa5853678d5
jonghwa <jonghwa0710@naver.com>
2019-08-11 22:59:18 +0900

    setting : 프로젝트 구조 셋팅

---
+++ server/routes/algorithmes/algorithmes.ctrl.js
@@ -0,0 +1,7 @@
+const index = (req, res, next) => {
+  res.end('algorithm idnex');
+};
+
+module.exports = {
+  index
+};
---
+++ server/routes/index.js
@@ -0,0 +1,26 @@
+const createError = require('http-errors');
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.use('/algorithmes', require('./algorithmes'));
+
+router.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+router.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = router;
