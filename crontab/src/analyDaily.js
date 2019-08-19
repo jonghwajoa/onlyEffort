166eb020291c8fba81f25f2fd8dbe63e1ecd3cfc
jonghwa <jonghwa0710@naver.com>
2019-08-19 12:30:09 +0900

    모듈화 : 분석 클래스 생성

--- crontab/src/analyDaily.js
+++ crontab/src/analyDaily.js
@@ -1,37 +1,54 @@
 const DB = require('../db');
 const API = require('./API');
-const moment = require('moment');
-const TODAY = moment().format('YYYYMMDD');
+const { TODAY, YESTERDAY, getCunrrentWeek } = require('../lib/Date');
 const bulkdata = require('./buldata');
 
 const api = new API();
+
 const dbConnect = async () => {
   process.env.NODE_ENV === 'development' ? await bulkdata() : await DB.sequelize.sync();
 };
 
-const compareWithYesterday = () => {};
-
-const getSolveProblem = async userIds => {
-  const todaySolveObj = {};
-  for (const user of userIds) {
-    const solveProblem = await api.getSolveProblem(user); // type : JSON
-    todaySolveObj[user] = solveProblem;
-  }
-  return todaySolveObj;
-};
-
+/**
+ * @param {Object} todaySolveObj
+ * Obj = {
+ *  solveProblem : {},
+ *  size
+ * }
+ */
 const saveSolveProblem = async todaySolveObj => {
-  for (const key in todaySolveObj) {
-    await DB.SolveProblem.createTodaySolve(key, todaySolveObj[key], TODAY);
+  for (const userId in todaySolveObj) {
+    await DB.SolveProblem.createTodaySolve(userId, todaySolveObj[userId], TODAY);
   }
 };
+
+class AnalysisSolveProblem {
+  constructor() {}
+
+  async getSolveProblem(userIds) {
+    const todaySolveObj = {};
+    for (const user of userIds) {
+      const solveProblem = await api.getSolveProblem(user); // type : JSON
+      todaySolveObj[user] = solveProblem;
+    }
+    return todaySolveObj;
+  }
+
+  async compareWithYesterday(todaySolveObj) {
+    for (const key in todaySolveObj) {
+      await DB.SolveProblem.createTodaySolve(key, todaySolveObj[key], TODAY);
+    }
+  }
+}
 
 const run = async () => {
   await dbConnect();
   const userIds = await DB.User.getAllUserId();
   const userIdArr = userIds.map(user => user.userId);
-  const todaySolveObj = await getSolveProblem(userIdArr);
+  const analysis = new AnalysisSolveProblem();
+  const todaySolveObj = await analysis.getSolveProblem(userIdArr);
   await saveSolveProblem(todaySolveObj);
+  compareWithYesterday(todaySolveObj);
 };
 
 try {
